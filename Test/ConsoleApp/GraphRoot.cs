9a2dd78a09e7888e0b6ea88c92deb86857e0ae2d
Áthila Naniny Ninow <athilann@gmail.com>
2023-11-09 14:19:17 +0000

    init

---
+++ Test/ConsoleApp/GraphRoot.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+public class GraphRoot
+{
+    public static bool IsGrowing { get; set; }
+    public List<Seed> Seeds { get; set; }
+
+    public GraphRoot()
+    {
+        Seeds = new List<Seed>();
+    }
+
+    public Task Grow()
+    {
+        return Task.Run(()=>
+        { 
+            var growList = new List<Task>();
+            foreach (var seed in Seeds)
+            {
+                growList.Add(seed.Grow(new Seed[]{seed}, Seeds.Count));
+            }
+        //    var seed = Seeds.FirstOrDefault();
+        //    growList.Add(seed.Grow(new Seed[]{seed}, Seeds.Count));
+           Task.WaitAll(growList.ToArray());
+        }); 
+    }
+
+
+}
---
+++ Test/ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+GraphRoot root = new GraphRoot();
+Seed seedA = new Seed("A");
+Seed seedB = new Seed("B");
+Seed seedC = new Seed("C");
+Seed seedD = new Seed("D");
+Seed seedE = new Seed("E");
+Seed seedF = new Seed("F");
+
+
+seedA.Connections.Add(seedB);
+seedA.Connections.Add(seedC);
+seedA.Connections.Add(seedF);
+
+seedB.Connections.Add(seedA);
+seedB.Connections.Add(seedC);
+seedB.Connections.Add(seedD);
+
+seedC.Connections.Add(seedA);
+seedC.Connections.Add(seedB);
+seedC.Connections.Add(seedD);
+seedC.Connections.Add(seedF);
+
+seedD.Connections.Add(seedB);
+seedD.Connections.Add(seedC);
+seedD.Connections.Add(seedF);
+seedD.Connections.Add(seedE);
+
+seedE.Connections.Add(seedD);
+seedE.Connections.Add(seedF);
+
+seedF.Connections.Add(seedA);
+seedF.Connections.Add(seedC);
+seedF.Connections.Add(seedD);
+seedF.Connections.Add(seedE);
+
+root.Seeds.Add(seedA);
+root.Seeds.Add(seedB);
+root.Seeds.Add(seedC);
+root.Seeds.Add(seedD);
+root.Seeds.Add(seedE);
+root.Seeds.Add(seedF);
+
+
+root.Grow().Wait();
+
+
